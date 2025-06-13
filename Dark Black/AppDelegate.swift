cda7d3a08668d80af72fd7d87bdf462600d19ec9
Bradly Feeley <bradlyf@gmail.com>
2025-06-13 15:32:34 -0700

    Initial commit.

--- Dark Black/AppDelegate.swift
+++ Dark Black/AppDelegate.swift
@@ -1,30 +1,88 @@
-//
-//  AppDelegate.swift
-//  Dark Black
-//
-//  Created by Bradly Feeley on 6/13/25.
-//
-
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+    private var statusItem: NSStatusItem?
+    private var overlayWindow: NSWindow?
+    private var isDarkModeEnabled = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        setupStatusBar()
+        hideFromDock()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
 
+    private func hideFromDock() {
+        NSApp.setActivationPolicy(.accessory)
+    }
 
+    private func setupStatusBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        guard let button = statusItem?.button else { return }
+        button.image = NSImage(systemSymbolName: "moon.fill", accessibilityDescription: "Dark Black")
+
+        let menu = NSMenu()
+
+        let toggleItem = NSMenuItem(title: "Enable Dark Mode", action: #selector(toggleDarkMode), keyEquivalent: "")
+        toggleItem.target = self
+        menu.addItem(toggleItem)
+
+        menu.addItem(NSMenuItem.separator())
+
+        let quitItem = NSMenuItem(title: "Quit Dark Black", action: #selector(quitApp), keyEquivalent: "q")
+        quitItem.target = self
+        menu.addItem(quitItem)
+
+        statusItem?.menu = menu
+    }
+
+    @objc private func toggleDarkMode() {
+        isDarkModeEnabled.toggle()
+        
+        if isDarkModeEnabled {
+            createOverlay()
+            statusItem?.menu?.item(at: 0)?.title = "Disable Dark Mode"
+        } else {
+            removeOverlay()
+            statusItem?.menu?.item(at: 0)?.title = "Enable Dark Mode"
+        }
+    }
+    
+    @objc private func quitApp() {
+        NSApp.terminate(nil)
+    }
+    
+    private func createOverlay() {
+        guard let screen = NSScreen.main else { return }
+        
+        overlayWindow = NSWindow(
+            contentRect: screen.frame,
+            styleMask: [.borderless],
+            backing: .buffered,
+            defer: false
+        )
+        
+        overlayWindow?.level = NSWindow.Level.screenSaver
+        overlayWindow?.backgroundColor = NSColor.black.withAlphaComponent(0.5)
+        overlayWindow?.isOpaque = false
+        overlayWindow?.hasShadow = false
+        overlayWindow?.ignoresMouseEvents = true
+        overlayWindow?.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+        
+        overlayWindow?.makeKeyAndOrderFront(nil)
+    }
+    
+    private func removeOverlay() {
+        overlayWindow?.orderOut(nil)
+        overlayWindow = nil
+    }
 }
-
