6d0881926ad5b5b3b7602a2f2980c958262e37e2
Bradly Feeley <bradlyf@gmail.com>
2025-06-13 21:11:42 -0700

    .

--- Dark Black/AppDelegate.swift
+++ Dark Black/AppDelegate.swift
@@ -2,42 +2,48 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     private var statusItem: NSStatusItem?
     private var overlayWindows: [NSWindow] = []
     private var isDarkModeEnabled = false
-
+    private var dimAmount: CGFloat = 0.5
+    private var popover: NSPopover?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         setupStatusBar()
         hideFromDock()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         
     }
-
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
+    
     private func hideFromDock() {
         NSApp.setActivationPolicy(.accessory)
     }
-
+    
     private func setupStatusBar() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-
+        
         guard let button = statusItem?.button else { return }
         button.image = NSImage(systemSymbolName: "curtains.open", accessibilityDescription: "Curtains")
-
+        
         let menu = NSMenu()
-
+        
         let toggleItem = NSMenuItem(title: "Draw Curtains", action: #selector(toggleDarkMode), keyEquivalent: "")
         toggleItem.target = self
         menu.addItem(toggleItem)
-
+        
+        let dimmerItem = NSMenuItem(title: "Adjust Dimming...", action: #selector(showDimmerPopover), keyEquivalent: "")
+        dimmerItem.target = self
+        menu.addItem(dimmerItem)
+        
         menu.addItem(NSMenuItem.separator())
-
+        
         let aboutItem = NSMenuItem(title: "About Curtains", action: #selector(showAbout), keyEquivalent: "")
         aboutItem.target = self
         menu.addItem(aboutItem)
@@ -45,10 +51,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let quitItem = NSMenuItem(title: "Quit Curtains", action: #selector(quitApp), keyEquivalent: "q")
         quitItem.target = self
         menu.addItem(quitItem)
-
+        
         statusItem?.menu = menu
     }
-
+    
+    @objc private func showDimmerPopover() {
+        guard let button = statusItem?.button else { return }
+        
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 200, height: 60)
+        popover.behavior = .transient
+        popover.contentViewController = createSliderViewController()
+        
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        self.popover = popover
+    }
+    
+    private func createSliderViewController() -> NSViewController {
+        let viewController = NSViewController()
+        let view = NSView(frame: NSRect(x: 0, y: 0, width: 200, height: 60))
+        
+        let slider = NSSlider(frame: NSRect(x: 20, y: 20, width: 160, height: 20))
+        slider.minValue = 0.1
+        slider.maxValue = 0.9
+        slider.doubleValue = Double(dimAmount)
+        slider.target = self
+        slider.action = #selector(sliderChanged(_:))
+        
+        view.addSubview(slider)
+        viewController.view = view
+        
+        return viewController
+    }
+    
+    @objc private func sliderChanged(_ sender: NSSlider) {
+        dimAmount = CGFloat(sender.doubleValue)
+        if isDarkModeEnabled {
+            updateOverlayOpacity()
+        }
+    }
+    
     @objc private func toggleDarkMode() {
         isDarkModeEnabled.toggle()
         
@@ -62,6 +104,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             statusItem?.button?.image = NSImage(systemSymbolName: "curtains.open", accessibilityDescription: "Curtains Open")
         }
     }
+    
     @objc private func showAbout() {
         let alert = NSAlert()
         alert.messageText = "Curtains"
@@ -92,7 +135,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             )
             
             window.level = NSWindow.Level.screenSaver
-            window.backgroundColor = NSColor.black.withAlphaComponent(0.5)
+            window.backgroundColor = NSColor.black.withAlphaComponent(dimAmount)
             window.isOpaque = false
             window.hasShadow = false
             window.ignoresMouseEvents = true
@@ -100,6 +143,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             
             window.orderFront(nil)
             overlayWindows.append(window)
+        }
+    }
+    
+    private func updateOverlayOpacity() {
+        for window in overlayWindows {
+            window.backgroundColor = NSColor.black.withAlphaComponent(dimAmount)
         }
     }
     
